0055980a5b8db836e90dbeaca5e8416db35f97e0
Alex <alexzhu@umich.edu>
2017-03-12 01:13:50 -0500

    Display alarm time in popup, cancel specific alarm feature

--- src/popup.js
+++ src/popup.js
@@ -6,16 +6,17 @@
 
 // Add listeners to html input forms and buttons.
 document.addEventListener("DOMContentLoaded", function () {
+    displayAlarms();
     var alarmInput = document.querySelector("#alarmInput");
-    var alarmCancel = document.getElementById("alarmCancel");
-    var alarmDisplay = document.getElementById("alarmDisplay");
-    if (!(alarmInput && alarmCancel && alarmDisplay)) {
+    var alarmCancel = document.querySelector("#alarmCancel");
+    var alarmCancelAll = document.getElementById("alarmCancelAll");
+    if (!(alarmInput && alarmCancel && alarmCancelAll)) {
         alert("Error: Could not find button from popup.html!");
         return;
     }
     alarmInput.addEventListener("keydown", createAlarm);
-    alarmCancel.addEventListener("click", cancelAlarms);
-    alarmDisplay.addEventListener("click", displayAlarms);
+    alarmCancel.addEventListener("keydown", cancelAlarm);
+    alarmCancelAll.addEventListener("click", cancelAllAlarms);
 });
 
 // Create alarm if user enters a valid float. Triggered on enter keydown in 
@@ -35,34 +36,50 @@ function createAlarm(e) {
     }
 }
 
+// Cancel a single alarm, given an id.
+function cancelAlarm(e) {
+    if (e.keyCode !== 13)
+        return;
+    id = document.getElementById("alarmCancel").value;
+    chrome.alarms.getAll(function (alarms) {
+        if (id <= 0 || alarms.length < id) {
+            alert("Alarm " + id + " does not exist!");
+            return;
+        }
+        chrome.alarms.clear(alarms[id - 1].name);
+    });
+}
+
+// Cancel all existing alarms.
+function cancelAllAlarms() {
+    chrome.alarms.clearAll();
+    alert("All alarms cancelled");
+}
+
+// Display remaining time for all existing alarms in popup body.
+function displayAlarms() {
+    setInterval(myTimer, 100);
+    function myTimer() {
+        chrome.alarms.getAll(function (alarms) {
+            var output = "";
+            if (!alarms.length)
+                output = "No alarms set."
+            else
+                for (var i = 0; i < alarms.length; i++) {
+                    var time_left = minutesLeft(alarms[i].scheduledTime);
+                    output += "Alarm " + (i + 1) + " - " + time_left + " min remaining\n";
+                }
+            document.getElementById("time").innerHTML = output;
+        });
+    }
+}
+
 // Given an alarm's scheduledTime, return remaining time in string format.
+// TODO: can replace this with Date methods (see showTime())
 function minutesLeft(scheduledTime) {
     var ms_left = scheduledTime - Date.now();
     var min = ms_left / 1000 / 60 << 0;
     var sec = ms_left / 1000 % 60 << 0;
     var separator = sec < 10 ? ":0" : ":";
     return min + separator + sec;
-}
-
-// Cancel all existing alarms.
-function cancelAlarms() {
-    chrome.alarms.clearAll();
-    alert("All alarms cancelled");
-}
-
-// Display all existing alarms.
-function displayAlarms() {
-    chrome.alarms.getAll(function (alarms) {
-        console.log("Displaying " + alarms.length + " alarms");
-        if (!alarms.length) {
-            alert("No active alarms.");
-            return;
-        }
-        var output = "";
-        for (var i = 0; i < alarms.length; i++) {
-            var time_left = minutesLeft(alarms[i].scheduledTime);
-            output += "Alarm " + i + " - " + time_left + " minutes remaining\n";
-        }
-        alert(output);
-    });
 }
